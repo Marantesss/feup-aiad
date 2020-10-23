c52d7c57f10afbe074ada6a065e1340bf69340da
Marantesss <up201706917@fe.up.pt>
2020-10-23 14:21:41 +0100

    build: add jade and new project folder to main

--- project1/src/main/java/Main.java
+++ project1/src/main/java/Main.java
@@ -13,9 +13,9 @@ public class Main {
 
         ContainerController cc = rt.createMainContainer(profile);
 
-        AgentController dev1 = cc.createNewAgent("developer1", "DeveloperAgent", null);
-        AgentController dev2 = cc.createNewAgent("developer2", "DeveloperAgent", null);
-        AgentController sm = cc.createNewAgent("ScrumMaster", "ScrumMasterAgent", null);
+        AgentController dev1 = cc.createNewAgent("developer1", "Agents.DeveloperAgent", null);
+        AgentController dev2 = cc.createNewAgent("developer2", "Agents.DeveloperAgent", null);
+        AgentController sm = cc.createNewAgent("ScrumMaster", "Agents.ScrumMasterAgent", null);
 
         dev1.start();
         dev2.start();
