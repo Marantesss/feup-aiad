bd9084cc6401b06ca03fca4e4d98d876beb6d443
simawatt <simaoxo@hotmail.com>
2020-10-30 15:48:33 +0000

    basic contract net setup

--- project1/src/main/java/Agents/DeveloperAgent.java
+++ project1/src/main/java/Agents/DeveloperAgent.java
@@ -3,6 +3,12 @@ package Agents;
 import Tasks.Task;
 import Tasks.TaskType;
 import jade.core.Agent;
+import jade.domain.FIPAAgentManagement.FailureException;
+import jade.domain.FIPAAgentManagement.NotUnderstoodException;
+import jade.domain.FIPAAgentManagement.RefuseException;
+import jade.lang.acl.ACLMessage;
+import jade.lang.acl.MessageTemplate;
+import jade.proto.ContractNetResponder;
 
 public class DeveloperAgent extends Agent {
     private static int count = 0;
@@ -18,7 +24,7 @@ public class DeveloperAgent extends Agent {
 
     @Override
     protected void setup() {
-        super.setup();
+        addBehaviour(new FIPAContractNetResp(this, MessageTemplate.MatchPerformative(ACLMessage.CFP)));
     }
 
     @Override
@@ -50,4 +56,39 @@ public class DeveloperAgent extends Agent {
                 ", aoe=" + aoe +
                 '}';
     }
+
+    static class FIPAContractNetResp extends ContractNetResponder {
+        FIPAContractNetResp(Agent a, MessageTemplate mt) {
+            super(a, mt);
+        }
+
+        @Override
+        protected ACLMessage handleCfp(ACLMessage cfp) throws RefuseException, FailureException, NotUnderstoodException {
+            ACLMessage reply = cfp.createReply();
+            reply.setPerformative(ACLMessage.PROPOSE);
+
+            // Attach the time it will end the proposed task
+            // reply.setContentObject();
+
+            return reply;
+        }
+
+        @Override
+        protected void handleRejectProposal(ACLMessage cfp, ACLMessage propose, ACLMessage reject) {
+            System.out.println(myAgent.getLocalName() + " got a reject...");
+        }
+
+        @Override
+        protected ACLMessage handleAcceptProposal(ACLMessage cfp, ACLMessage propose, ACLMessage accept) throws FailureException {
+            System.out.println(myAgent.getLocalName() + " got an accept!");
+
+            ACLMessage result = accept.createReply();
+            // Verify between all the informs
+            result.setPerformative(ACLMessage.INFORM);
+            result.setContent("this is the result");
+
+            return result;
+        }
+    }
+
 }
