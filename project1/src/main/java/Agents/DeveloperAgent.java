ede1a9167dc4967cf261970efd8eef404460d0fd
Leonardo Moura <leofmoura17@gmail.com>
2020-11-06 16:22:55 +0000

    :construction: Added initial developer task allocation

--- project1/src/main/java/Agents/DeveloperAgent.java
+++ project1/src/main/java/Agents/DeveloperAgent.java
@@ -4,16 +4,40 @@ import Tasks.Task;
 import Tasks.TaskType;
 import jade.core.Agent;
 
+import java.util.HashMap;
+
 public class DeveloperAgent extends Agent {
     private static int count = 0;
 
     private final int id;
-    private Task task;
     private final TaskType aoe;
+    private final HashMap<Task,Integer> tasks; //key -> task; Value: -> instant a task is finished
+    private Task latestTask; //The latest task this developer is working on
+
+    //TODO: Add aoe
+    //TODO: Comment functions
+    //TODO: Create proposal
 
     public DeveloperAgent(TaskType aoe) {
         this.id = ++count;
         this.aoe = aoe;
+        this.tasks = new HashMap<>();
+    }
+
+    /**
+     * Returns the minimum instant a task can be allocated to
+     * @param task The task to allocate
+     * @return The minimum instant this developer can start the task
+     */
+    private int allocateTask(Task task) {
+        if (latestTask == null) {
+            return 0;
+        }
+
+        //Get the earliest theoretical instant the task can be finished
+        int earlyInstant = tasks.get(latestTask) + latestTask.getDuration();
+
+        return Math.max(earlyInstant, task.getStartingInstant());
     }
 
     @Override
@@ -30,12 +54,19 @@ public class DeveloperAgent extends Agent {
         return id;
     }
 
-    public Task getTask() {
-        return task;
+    /**
+     * Returns the task Hashmap of the developer
+     * @return
+     */
+    public HashMap<Task, Integer> getTasks() {
+        return  this.tasks;
     }
 
-    public void setTask(Task task) {
-        this.task = task;
+    public void addTask(Task task) {
+        //Get instant the task can be started
+        int instant = allocateTask(task);
+
+        tasks.put(task,instant);
     }
 
     public TaskType getAoe() {
@@ -46,7 +77,7 @@ public class DeveloperAgent extends Agent {
     public String toString() {
         return "DeveloperAgent{" +
                 "id=" + id +
-                ", task=" + task +
+                ", tasks=" + tasks + //TODO: print tasks instead of pointer
                 ", aoe=" + aoe +
                 '}';
     }
