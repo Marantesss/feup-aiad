57e6a937dac8ae39b3c62b866425746eb9f5d0b6
Marantesss <up201706917@fe.up.pt>
2020-11-06 14:53:05 +0000

    docs: write some documentation for the strategy interface

--- project1/src/main/java/agents/strategies/Strategy.java
+++ project1/src/main/java/agents/strategies/Strategy.java
@@ -1,4 +1,21 @@
 package agents.strategies;
 
+/**
+ * Given a task to complete and a set of developers, the scrum master
+ * will ask all developers when will this task be completed if it
+ * were allocated to them. Then it will choose the best developer.
+ *
+ * To choose the best developer, the scrum master will receive
+ * information about the developer (identification) and the
+ * task (time until the task is completed)
+ *
+ * This time until the task is completed is calculated
+ * by the time it takes to complete all allocated tasks to
+ * that developer plus this newly added task.
+ *
+ * Given this information from all developers, the scrum master will
+ * use a given strategy to choose which developer will get
+ * the task.
+ */
 public interface Strategy {
 }
