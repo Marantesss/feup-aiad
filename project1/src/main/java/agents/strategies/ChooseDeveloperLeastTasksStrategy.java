8364d6421ea5e440140270824d45630150c4be33
simawatt <simaoxo@hotmail.com>
2020-11-13 19:19:20 +0000

    feat: choose developer based on number of tasks. low=better

--- project1/src/main/java/agents/strategies/ChooseDeveloperLeastTasksStrategy.java
+++ project1/src/main/java/agents/strategies/ChooseDeveloperLeastTasksStrategy.java
@@ -1,25 +1,23 @@
 package agents.strategies;
 
 import proposals.Proposal;
-import proposals.comparators.ProposalTaskQuantityComparator;
 
-import java.util.Comparator;
 import java.util.List;
 
 /**
  * This strategy chooses the best proposal based on the number of tasks it is doing
  */
 public class ChooseDeveloperLeastTasksStrategy implements ChooseDeveloperStrategy {
-
-    /**
-     * This comparator will sort the proposals based on the number of tasks
-     */
-    private final Comparator<Proposal> comparator = new ProposalTaskQuantityComparator();
-
     @Override
     public Proposal execute(List<Proposal> proposals) {
-        proposals.sort(comparator);
+        int min = proposals.get(0).getNumberOfTasks();
+        Proposal ret = proposals.get(0);
 
-        return proposals.get(0);
+        for (Proposal p : proposals) {
+            if(p.getNumberOfTasks() < min)
+                ret = p;
+        }
+
+        return ret;
     }
 }
