f3b2d2f868af0d4eb10f389a42dd1c7b63bdcddf
Marantesss <up201706917@fe.up.pt>
2020-11-14 20:59:58 +0000

    refactor: update config json files to json/ folder in root deleted unnecessary results folder in main

--- project1/run.sh
+++ project1/run.sh
@@ -4,4 +4,4 @@
 javac -cp lib/gson-2.8.6.jar:lib/jade.jar  -d out  src/main/java/Main.java src/main/java/*/*.java src/main/java/*/*/*.java
 
 #Run main method
-java -cp lib/jade.jar:lib/gson-2.8.6.jar:out Main src/main/resources/config.test.json
+java -cp lib/jade.jar:lib/gson-2.8.6.jar:out Main json/config.test.json
